0df79f5fca4479dea175f65f10596b6ad4ab5a13
asuforce <asuforcegt@hotmail.co.jp>
2016-06-16 15:16:16 +0900

    保存し忘れたファイルを追

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get new" do
     get :new
     assert_response :success
