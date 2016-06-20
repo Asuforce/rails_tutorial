f108e812f7f0c8e311cbf9573becc00fa6e25d49
asuforce <asuforcegt@hotmail.co.jp>
2016-06-20 18:22:37 +0900

    fix typo

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
@@ -8,5 +9,5 @@ port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveRecord:: Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
