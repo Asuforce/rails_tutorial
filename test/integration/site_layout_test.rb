43c28ddc724d0299c52a2b5522a00cab79a71764
asuforce <asuforcegt@hotmail.co.jp>
2016-06-27 12:40:05 +0900

    test追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,6 +13,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", user_path(@admin)
+    assert_select "a[href=?]", edit_user_path(@admin)
+    assert_select "a[href=?]", logout_path
     assert_select "a.dropdown-toggle"
   end
 
