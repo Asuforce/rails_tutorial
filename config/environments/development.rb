dd871145ab3c962c37b2f544bb5914f922391066
asuforce <asuforcegt@hotmail.co.jp>
2016-06-29 14:31:58 +0900

    setting mail activity environment for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,10 +15,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.lolipop.jp',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['LOLIPOP_USERNAME'],
+    :password => ENV['LOLIPOP_PASSWORD'],
+    :domain => 'asuforce.xyz',
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
