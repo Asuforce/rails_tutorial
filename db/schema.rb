9af4cf63310d4ddbd21b46d0672dd7ceab74e3f9
asuforce <asuforcegt@hotmail.co.jp>
2016-06-29 18:37:27 +0900

    update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160627085801) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.boolean  "activated"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
