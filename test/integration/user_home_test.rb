f4fcb0d1455cd4e36af9651e4e6d6dee31868cc4
asuforce <asuforcegt@hotmail.co.jp>
2016-07-05 15:41:37 +0900

    add user_home test

---
+++ test/integration/user_home_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UserHomeTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "home display with login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'title', full_title()
+    assert_select 'h1', text: @user.name
+    assert_select 'img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_select 'a[href=?]', followers_user_path(@user)
+    assert_match @user.followers.count.to_s, response.body
+  end
+end
