f1e04ebfa300ad193200fd9cb39e58edbf3b0f98
asuforce <asuforcegt@hotmail.co.jp>
2016-07-06 17:52:08 +0900

    add json response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,19 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    redirect_to root_url and return unless @user.activated?
+    respond_to do |format|
+      format.html { redirect_to root_url and return unless @user.activated? }
+      format.json
+    end
   end
 
   def new
@@ -19,47 +26,68 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
-    else
-       render 'new'
+    respond_to do |format|
+      if @user.save
+        @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        format.html { redirect_to root_url }
+        format.json { head :no_content, status: :ok }
+      else
+         format.html { render 'new' }
+         format.json { render json: @user.errors }
+      end
     end
   end
 
   def edit
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        format.html { redirect_to @user }
+        format.json { head :no_content, status: :ok }
+      else
+        format.html { render 'edit'}
+        format.json { render json: @user.errors, status: :unprocessable_entry }
+      end
     end
   end
 
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content, status: :ok }
+    end
   end
 
   def following
     @title = "Following"
     @user = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
+    respond_to do |format|
+      format.html { render 'show_follow' }
+      format.json { render 'show_follow' }
+    end
   end
 
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
+    respond_to do |format|
+      format.html { render 'show_follow' }
+      format.json { render 'show_follow' }
+    end
   end
 
   private
