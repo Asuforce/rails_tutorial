1a2975fd63f89ae5306bb47f6e817160dea26b06
asuforce <asuforcegt@hotmail.co.jp>
2016-07-07 15:15:36 +0900

    add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,15 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
-  resources :users
+
+  namespace :api, { format: 'json' } do
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
