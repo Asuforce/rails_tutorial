f1be898679df0b80bde03fead496d922d2f7b65c
asuforce <asuforcegt@hotmail.co.jp>
2016-07-13 17:29:53 +0900

    delete doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,12 @@ Rails.application.routes.draw do
         get :following, :followers
       end
     end
+    post 'login' => 'sessions#create'
+    delete 'logout' => 'sessions#destroy'
   end
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-
-  use_doorkeeper
 end
