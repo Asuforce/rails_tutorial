96505be605b0a6fb67e57029051bd307b2f83f1f
asuforce <asuforcegt@hotmail.co.jp>
2016-07-13 11:13:36 +0900

    fix api test

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -19,16 +19,12 @@ class Api::UsersControllerTest < ActionController::TestCase
 
     users = User.where(activated: true).paginate(page: 1)
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    json_ids = json[:users].map{|user| user[:id]}
-    json_names = json[:users].map{|user| user[:name]}
-    json_emails = json[:users].map{|user| user[:email]}
-
-    users.each do |user|
-      assert json_ids.include?(user[:id])
-      assert json_names.include?(user[:name])
-      assert json_emails.include?(user[:email])
-    end
+    pattern = {
+      users: users.map {|user| {
+        id: user[:id], name: user[:name], email: user[:email]
+      }}.ordered!
+    }
+    assert_json_match pattern, response.body
   end
 
   test "should get show success responce and correct json response" do
@@ -38,19 +34,17 @@ class Api::UsersControllerTest < ActionController::TestCase
 
     microposts = @user.microposts.paginate(page: 1)
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    json_user = json[:user]
-    json_ids = json[:feeds][:microposts].map{|micropost| micropost[:id]}
-    json_contents = json[:feeds][:microposts].map{|micropost| micropost[:content]}
-
-    assert_equal @user.id, json_user[:id]
-    assert_equal @user.name, json_user[:name]
-
-    microposts.each do |micropost|
-      assert json_ids.include?(micropost[:id])
-      assert json_contents.include?(micropost[:content])
-      assert_equal @user.id, micropost[:user_id]
-    end
+    pattern = {
+      user: {
+        id: @user.id, name: @user.name
+      },
+      feeds: {
+        microposts: microposts.map {|micropost| {
+          id: micropost[:id], content: micropost[:content], user_id: micropost[:user_id]
+        }}.ordered!
+      }
+    }
+    assert_json_match pattern, response.body
   end
 
   test "invalid signup information" do
@@ -96,6 +90,7 @@ class Api::UsersControllerTest < ActionController::TestCase
     log_in_as(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
+
     patch :update, id: @user, format: :json,
       user: {
         name: name,
@@ -144,20 +139,17 @@ class Api::UsersControllerTest < ActionController::TestCase
 
     users = @user.following.paginate(page: 1)
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    json_user = json[:user]
-    json_ids = json[:following][:users].map{|user| user[:id]}
-    json_names = json[:following][:users].map{|user| user[:name]}
-    json_imges = json[:following][:users].map{|user| user[:img]}
-
-    assert_equal @user.id, json_user[:id]
-    assert_equal @user.name, json_user[:name]
-
-    users.each do |user|
-      assert json_ids.include?(user[:id])
-      assert json_names.include?(user[:name])
-      assert json_imges.include?(gravatar_url(user, size: 50))
-    end
+    pattern = {
+      user: {
+        id: @user.id, name: @user.name
+      }.ignore_extra_keys!,
+      following: {
+        users: users.map {|user| {
+          id: user[:id], name: user[:name], img: gravatar_url(user, size: 50)
+        }}.ordered!
+      }
+    }
+    assert_json_match pattern, response.body
   end
 
   test "should not get followers success responce" do
@@ -172,19 +164,16 @@ class Api::UsersControllerTest < ActionController::TestCase
 
     users = @user.followers.paginate(page: 1)
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    json_user = json[:user]
-    json_ids = json[:followers][:users].map{|user| user[:id]}
-    json_names = json[:followers][:users].map{|user| user[:name]}
-    json_imges = json[:followers][:users].map{|user| user[:img]}
-
-    assert_equal @user.id, json_user[:id]
-    assert_equal @user.name, json_user[:name]
-
-    users.each do |user|
-      assert json_ids.include?(user[:id])
-      assert json_names.include?(user[:name])
-      assert json_imges.include?(gravatar_url(user, size: 50))
-    end
+    pattern = {
+      user: {
+        id: @user.id, name: @user.name
+      }.ignore_extra_keys!,
+      followers: {
+        users: users.map {|user| {
+          id: user[:id], name: user[:name], img: gravatar_url(user, size: 50)
+        }}.ordered!
+      }
+    }
+    assert_json_match pattern, response.body
   end
 end
