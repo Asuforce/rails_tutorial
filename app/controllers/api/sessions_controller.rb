88a6f7281392fa86b9fb9a6fb4410bc56ffc6320
asuforce <asuforcegt@hotmail.co.jp>
2016-07-15 11:22:21 +0900

    update jwt processing

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,6 +4,7 @@ class Api::SessionsController < Api::ApplicationController
     @user = User.find_by(email: params[:email].downcase)
     if @user && @user.authenticate(params[:password])
       if @user.activated?
+        @token = create_jwt
         render status: :created
       else
         render json: user.errors.messages, status: :unprocessable_entity
@@ -12,4 +13,14 @@ class Api::SessionsController < Api::ApplicationController
       render json: user.errors.messages, status: :unprocessable_entity
     end
   end
+
+  private
+
+    def create_jwt
+      exp = Time.now.to_i + 2 * 3600
+      iss = 'asuforcerails.herokuapp.com'
+      payload = {id: @user.id, exp: exp, iss: iss}
+      key = Rails.application.secrets[:secret_key_base]
+      JWT.encode payload, key, 'HS256'
+    end
 end
