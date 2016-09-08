2ea8440a2c3eff47a3c03fead9b21ce8a5b7ea80
Homma Takuma <takuma.honma@pepabo.com>
2016-09-08 14:41:29 +0900

    listen先ポートとpidの場所を修正

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,8 +3,8 @@ root = File.expand_path('../../../', __FILE__)
 worker_processes 2
 working_directory root
 
-listen "#{root}/tmp/unicorn.sock"
-pid "#{root}/tmp/unicorn.pid"
+listen 3000
+pid "#{root}/tmp/pids/unicorn.pid"
 
 stderr_path "#{root}/log/unicorn_error.log"
 stdout_path "#{root}/log/unicorn.log"
