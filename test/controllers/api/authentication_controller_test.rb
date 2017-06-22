b9c2e3c31eefc818005b936855c0e04d90697b9c
asuforce <asuforcegt@hotmail.co.jp>
2017-06-22 19:27:03 +0900

    fix token parameter

--- test/controllers/api/authentication_controller_test.rb
+++ test/controllers/api/authentication_controller_test.rb
@@ -14,24 +14,24 @@ class Api::AuthenticationControllerTest < ActionController::TestCase
   end
 
   test "should fail create with non-activated user" do
-    post :create, params: {}, format: :json, email: @non_activate_user , password: 'password'
+    post :create, params: { email: @non_activate_user , password: 'password' }, format: :json
     assert_response :unprocessable_entity
   end
 
   test "should fail create with invalid password" do
-    post :create, params: {}, format: :json, email: @user.email, password: ''
+    post :create, params: { email: @user.email, password: '' }, format: :json
     assert_response :unprocessable_entity
   end
 
   test "should success create and correct json response" do
-    post :create, params: {}, format: :json, email: @user.email, password: 'password'
+    post :create, params: { email: @user.email, password: 'password' }, format: :json
     assert_response :success
 
     pattern = {
       user: {
         id: @user.id, name: @user.name, email: @user.email
       },
-      token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NzYyMTQ2MTExLCJleHAiOjE0NjY2MTQ4MDAsImlzcyI6ImFzdWZvcmNlcmFpbHMuaGVyb2t1YXBwLmNvbSJ9.iywYj7cwmmpZvI5v1wt4-j3e2mBzRVeX2fWiBgBxRXU"
+      token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NzYyMTQ2MTExLCJleHAiOjE0NjY2MTQ4MDAsImlzcyI6ImFzdWZvcmNlcmFpbHMuaGVyb2t1YXBwLmNvbSJ9.t3O0jl9rRayaX8hz9SUIPZRSQMmxqv-C6mUYvgWKZUo"
     }
     assert_json_match pattern, response.body
   end
